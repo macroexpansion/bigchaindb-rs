3c1c265676186e27872dfe89a0eeb635d36c7196
macroexpansion <trasuadev@gmail.com>
2024-01-20 01:09:40 +0700

    feat: impl Ed25519Sha256Fulfillment and FulfillmentChoice

--- crypto-conditions/src/schema.rs
+++ crypto-conditions/src/schema.rs
@@ -34,3 +34,58 @@ pub mod fingerprint {
         }
     }
 }
+
+pub mod fulfillment {
+    use asn1::*;
+
+    #[derive(Asn1Write)]
+    pub struct Ed25519Sha256Fulfillment<'a> {
+        #[implicit(0)]
+        pub public_key: Option<&'a [u8]>,
+        #[implicit(1)]
+        pub signature: Option<&'a [u8]>,
+    }
+
+    #[derive(Asn1Write)]
+    pub enum FulfillmentChoice<'a> {
+        // TODO: implement other choice
+        // preimageSha256Fulfillment: this.implicit(0).use(PreimageFulfillment),
+        // prefixSha256Fulfillment: this.implicit(1).use(PrefixFulfillment),
+        // thresholdSha256Fulfillment: this.implicit(2).use(ThresholdFulfillment),
+        // rsaSha256Fulfillment: this.implicit(3).use(RsaSha256Fulfillment),
+        #[implicit(4)]
+        Ed25519Sha256Fulfillment(Ed25519Sha256Fulfillment<'a>),
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_ed25519sha256_fulfillment() {
+            let bytes = [1u8; 32];
+
+            let fulfillment = Ed25519Sha256Fulfillment {
+                public_key: Some(&bytes[..]),
+                signature: Some(&bytes[..]),
+            };
+
+            let buffer = write_single(&fulfillment).unwrap();
+
+            assert_eq!(bs58::encode(buffer).into_string(), "382G1e9VCNbaKW7vC3hW2zzySF2uofrQNGV1fNvpq8xbysNitah3euMnpvrQLjmASbz5CQyF7iSqfdaMr1Ds3BvynZyuBRrx");
+        }
+
+        #[test]
+        fn test_fulfillment() {
+            let bytes = [1u8; 32];
+            let fulfillment = Ed25519Sha256Fulfillment {
+                public_key: Some(&bytes[..]),
+                signature: Some(&bytes[..]),
+            };
+            let choice = FulfillmentChoice::Ed25519Sha256Fulfillment(fulfillment);
+            let buffer = write_single(&choice).unwrap();
+
+            assert_eq!(bs58::encode(buffer).into_string(), "8DgD5ZffDSjWD1EyfqNwv5WkRJduNNo9YQvpLpu9akNzYM5tryitvi7yv9DSAh2kXouC8FDqWJkFtUXzkLoxiJoSofTJBsex")
+        }
+    }
+}
