08a1e18cd3510064b38f06b883ca5c155a89d95a
macroexpansion <trasuadev@gmail.com>
2024-01-24 18:55:34 +0700

    test post_transaction_commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-use bigchaindb;
+use std::collections::HashMap;
 
-fn main() {
-    let keypair = bigchaindb::ed25519_keypair();
-    println!("{keypair:?}")
+use bigchaindb::{
+    connection::Connection,
+    transaction::{AssetDefinition, Operation, TransactionTemplate},
+};
+
+#[tokio::main]
+async fn main() {
+    let nodes = "http://localhost:3000/".to_string();
+    let mut conn = Connection::new(vec![Some(nodes)], HashMap::new(), None);
+
+    let asset = serde_json::json!({
+        "ft": {
+            "signature": "signature",
+            "device": "device",
+        }
+    });
+    let metadata = serde_json::json!({"metadata": "metadata"});
+
+    let asset = AssetDefinition { data: Some(asset) };
+
+    let mut transaction = TransactionTemplate::new();
+    transaction.asset = Some(asset);
+    transaction.metadata = Some(metadata);
+    transaction.operation = Some(Operation::CREATE);
+
+    let _ = conn.post_transaction_commit(transaction).await;
 }
